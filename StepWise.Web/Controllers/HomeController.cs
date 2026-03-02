204ae5c3bbf3a7c4c80eacd2d87f23f892dd29cc
Irena07 <irra.nalbantova@gmail.com>
2026-03-02 19:47:46 +0200

    login

--- StepWise.Web/Controllers/HomeController.cs
+++ StepWise.Web/Controllers/HomeController.cs
@@ -26,7 +26,12 @@ namespace StepWise.Web.Controllers
             Response.Cookies.Append(
                 CookieRequestCultureProvider.DefaultCookieName,
                 CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    IsEssential = true,
+                    Path = "/"
+                }
             );
 
             return LocalRedirect(returnUrl);
